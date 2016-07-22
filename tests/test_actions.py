4b3ce0527678846b348ec06a0e80669c99a72028
Matthew Parrott <mparrott@teslamotors.com>
2016-07-21 20:13:18 -0700

    test for explicit name.

--- tests/test_actions.py
+++ tests/test_actions.py
@@ -13,6 +13,16 @@ def test_attaching_action():
 
     assert some_get.run is run
 
+    @endpoint.get()
+    def some_get_b():
+        return 'bar'
+
+    @some_get_b.endpoint.action(name='doit')
+    def run_2():
+        return 'yop'
+
+    assert some_get_b.doit is run_2
+
 
 def test_action_endpoint(basics, pyramid_app):
     r = pyramid_app.post('/api/v1/path/to/somewhere')
